ee253fc5ee2eed1d793bb36b5f5221e38328bb85
39320 <39320@DESKTOP-IJMGSHS>
2024-01-25 19:04:27 +0100

    Aggiungere i file di progetto.

---
+++ rettangolo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rettangolo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            rettangolo rettangolo= new rettangolo();
+
+            rettangolo.L1 = 3;
+            rettangolo.L2 = 4;
+
+            Console.WriteLine("{0}",rettangolo.calcoloarea(rettangolo.area));
+
+            Console.WriteLine("{0}", rettangolo.calcolaperimetro(rettangolo.perimetro));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ rettangolo/rettangolo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rettangolo
+{
+    internal class rettangolo
+    {
+        public int L1 { get; set; }
+
+        public int L2 { get; set; }
+
+        public int area { get; set; }
+
+        public int perimetro { get; set; }
+       
+        
+        public int calcoloarea(int area)
+        {
+            area = L1 * L2;
+
+            return area;
+        }
+        
+        public int calcolaperimetro(int perimetro)
+        {
+            perimetro = (L1 + L2) *2;
+
+            return perimetro;
+        }
+    }
+}
